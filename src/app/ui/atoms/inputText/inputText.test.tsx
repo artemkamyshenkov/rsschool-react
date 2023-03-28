7486b89130c9be28c60ca6aa9f2f2d4e6c7a3a61
Artem Kamyshenkov <artmiller22831@gmail.com>
2023-03-28 21:05:31 +0300

    fix test

--- src/app/ui/atoms/inputText/inputText.test.tsx
+++ src/app/ui/atoms/inputText/inputText.test.tsx
@@ -11,34 +11,15 @@ describe('Search input', () => {
     localStorage.clear();
   });
 
-  // it('saves input value to local storage on component unmount', () => {
-  //   const { getByTestId, rerender } = render(
-  //     <InputText value="" onChange={() => {}} placeholder="" />
-  //   );
-  //   const searchInput = getByTestId('search-input');
-
-  //   fireEvent.change(searchInput, { target: { value: 'new value' } });
-  //   expect(localStorage.getItem('searchInputValue')).toBeNull();
-
-  //   rerender(<InputText value="new value" onChange={() => {}} placeholder="" />);
-  //   expect(localStorage.getItem('searchInputValue')).toBeNull();
-
-  //   fireEvent(window, new Event('beforeunload'));
-  //   expect(localStorage.getItem('searchInputValue')).toBe('new value');
-  // });
-
   it('renders search input with placeholder', () => {
     render(<InputText className="" placeholder="Search..." onChange={() => {}} value="" />);
-
     expect(screen.getByPlaceholderText('Search...')).toBeInTheDocument();
   });
 
-  // it('onChange works', async () => {
-  //   render(<InputText value="" onChange={onChange} placeholder="Search" />);
-
-  //   const input = screen.getByRole('textbox');
-  //   userEvent.type(input, 'React');
-
-  //   await waitFor(() => expect(onChange).toHaveBeenCalledTimes(5));
-  // });
+  it('onChange works', async () => {
+    render(<InputText value="" onChange={onChange} placeholder="Search" className="" />);
+    const input = screen.getByRole('textbox');
+    userEvent.type(input, 'React');
+    await waitFor(() => expect(onChange).toHaveBeenCalledTimes(5));
+  });
 });
