452cf23b6ac84d7c3c5152db3e999b22254bd514
Artem Kamyshenkov <artmiller22831@gmail.com>
2023-04-01 22:10:47 +0300

    feat: create new for,

--- src/app/ui/atoms/input/input.types.ts
+++ src/app/ui/atoms/input/input.types.ts
@@ -2,6 +2,6 @@ import { InputHTMLAttributes } from 'react';
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   placeholder: string;
   className: string;
-  value: string;
+  value: string | number;
   type?: string;
 }
