ce63a684f0572a705a0882e0a4df6235c58b8f35
Artem Kamyshenkov <artmiller22831@gmail.com>
2023-04-20 13:36:35 +0300

    fix ssr

--- server.ts
+++ server.ts
@@ -21,10 +21,18 @@ async function createServer() {
     try {
       let template = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf-8');
       template = await vite.transformIndexHtml(url, template);
+      const html = template.split(`<!--ssr-outlet-->`);
       const { render } = await vite.ssrLoadModule('/src/entry-server.tsx');
-      const appHtml = await render(url);
-      const html = template.replace(`<!--ssr-outlet-->`, appHtml);
-      res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
+      const { pipe } = await render(url, {
+        onShellReady() {
+          res.write(html[0]);
+          pipe(res);
+        },
+        onAllReady() {
+          res.write(html[0] + html[1]);
+          res.end();
+        },
+      });
     } catch (e) {
       const err = e as Error;
       vite.ssrFixStacktrace(err);
@@ -35,4 +43,4 @@ async function createServer() {
 }
 
 createServer();
-console.log(`Server is running on http://localhost:${port}/`);
+console.log(`Server is running on http://localhost:${port}/ click to open`);
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Link, NavLink, Navigate } from 'react-router-dom';
 import MainPage from './pages/mainPage/mainPage';
 import AboutUs from './pages/aboutUs/aboutUs';
 import ErrorPage from './pages/errorPage/errorPage';
